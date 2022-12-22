fd4090203510c181c85f59ce5b058fd880b31e77
Marya Adaratskih <marya.adaratskih@yandex.ru>
2022-12-23 02:42:26 +0300

    change work

--- main.py
+++ main.py
@@ -5,37 +5,6 @@ from settings import TOKEN_Y, TOKEN_VK
 import time
 from tqdm import tqdm
 
-def largest_size(sizes):
-    sizes_list = ['w', 'z', 'y', 'x', 'm', 's']
-    for item in sizes_list:
-        for size in sizes:
-            if size['type'] == item:
-                return size
-
-
-def create_file_names(photos):
-    for photo in photos:
-        photo.name = str(photo.likes)
-        photo.date = datetime.fromtimestamp(photo.date).strftime('%Y-%m-%d')
-        if [p.likes for p in photos].count(photo.likes) > 1:
-            photo.name += '_' + str(photo.date)
-        photo.name += '.jpg'
-
-
-class Photo:
-    name = ''
-
-    def __init__(self, date, likes, sizes):
-        self.date = date
-        self.likes = likes
-        self.sizes = sizes
-        self.size_type = sizes['type']
-        self.url = sizes['url']
-        self.maxsize = max(sizes['width'], sizes['height'])
-
-    def __repr__(self):
-        return f'date: {self.date}, likes: {self.likes}, size: {self.maxsize}, url: {self.url}'
-
 
 class VK:
     url = 'https://api.vk.com/method/'
@@ -55,54 +24,71 @@ class VK:
             'extended': 1,
             'rev': 1
         }
-        resp = requests.get(get_url, params={**self.params, **params}).json().get('response').get('items')
-        return sorted([Photo(photo.get('date'),
-                             photo.get('likes')['count'],
-                             largest_size(photo.get('sizes'))) for photo in resp],
-                      key=lambda p: p.maxsize, reverse=True)[:count]
+        response = requests.get(get_url, params={**self.params, **params})
+        photo_profile = []
+        if response.status_code == 200:
+            for photo in response.json()['response']['items']:
+                photo_profile.append(
+                    {
+                        'likes': photo['likes']['count'],
+                        'type': photo['sizes'][-1]['type'],
+                        'url': photo['sizes'][-1]['url'],
+                        'date': datetime.fromtimestamp(photo['date']).strftime('%d-%m-%Y')
+                    }
+                )
+            return photo_profile[:count]
+        else:
+            print(f'Ошибка {response.status_code}')
 
 
 class Yandex:
 
-    def __init__(self, token: str):
+    def __init__(self, token: str, url_downloadable_files):
         self.token = token
+        self.url_downloadable_files = url_downloadable_files
 
     def create_folder(self, folder_name):
         url = "https://cloud-api.yandex.net/v1/disk/resources"
         headers = {"Authorization": self.token}
-        if requests.get(url, headers=headers, params={"path": folder_name}).status_code != 200:
-            requests.put(url, params={"path": folder_name}, headers=headers)
+        response = requests.put(url, params={"path": folder_name}, headers=headers)
+        if response.status_code == 201:
             print(f'\nПапка "{folder_name}" успешно создана в корневом каталоге Яндекс диска.\n')
-
         else:
-            print(f'\nПапка "{folder_name}" уже существует. Файлы могут быть загружены повторно.\n')
+            print(f' {response.json().get("message")} Файлы могут быть загружены повторно. '
+                  f'Status code: {response.status_code}')
         return folder_name
 
-    def upload(self, id_, photos):
-        folder_name = input('Введите имя папки: ')
-        create_file_names(photos)
-        if self.create_folder(folder_name):
-            result = []
-            for photo in tqdm(photos):
-                response = requests.post("https://cloud-api.yandex.net/v1/disk/resources/upload",
-                                         params={"path": '/' + folder_name + '/' + photo.name,
-                                                 "url": photo.url},
-                                         headers={"Authorization": self.token})
-                time.sleep(1)
-                if response.status_code == 202:
-                    result.append({"file_name": photo.name, "size": photo.size_type})
-                else:
-                    print(f'Ошибка загрузки "{photo.name}": '
-                          f'{response.json().get("message")}. Status code: {response.status_code}')
-            print(f'В папку "{folder_name}" загружено {len(result)} фото.')
-            with open(f'{id_}_{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}_files.json', "w") as f:
-                json.dump(result, f, ensure_ascii=False, indent=2)
+    def upload(self, id_, folder_name):
+        name_list = []
+        result = []
+        headers = {"Authorization": self.token}
+        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        for urls in tqdm(self.url_downloadable_files):
+            if urls['likes'] not in name_list:
+                result.append({'file_name': str(urls['likes']) + '.jpg', 'size': urls['type']})
+                name_list.append(urls['likes'])
+                path = '/' + folder_name + '/' + str(urls['likes']) + '.jpg'
+            else:
+                result.append({'file_name': str(urls['likes']) + f"_{urls['date']}" + '.jpg', 'size': urls['type']})
+                name_list.append(urls["likes"])
+                path = '/' + folder_name + '/' + str(urls['likes']) + f"_{urls['date']}" + '.jpg'
+            params = {"path": path, "url": urls["url"]}
+            time.sleep(1)
+            response = requests.post(url, headers=headers, params=params)
+            if response.status_code == 202:
+                pass
+            else:
+                print(f'Ошибка загрузки {response.json().get("message")}. Status code: {response.status_code}')
+        print(f'Загрузка завершена. В папку "{folder_name}" загружено {len(result)} фото.')
+        with open(f'{id_}_{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}_files.json', "w") as f:
+            json.dump(result, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == '__main__':
-    token_VK = TOKEN_VK
     vk_id = input('\nВведите id профиля, с которого хотите загрузить фотографии:\n')
-    counter_need_photo = int(input('\nВведите количество фотографий для скачивания (по умолчанию 5):\n'))
+    counter_need_photo = input('\nВведите количество фотографий для скачивания (по умолчанию 5):\n')
     vk_user = VK(TOKEN_VK, '5.131')
-    ya_user = Yandex(TOKEN_Y)
-    ya_user.upload(vk_id, vk_user.get_photos(vk_id, int(counter_need_photo)))
+    ya_user = Yandex(TOKEN_Y, vk_user.get_photos(vk_id, int(counter_need_photo)))
+    name_folder = ya_user.create_folder('VK_PHOTO')
+    ya_user.upload(vk_id, name_folder)
+
